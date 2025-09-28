205eab6733184b0c602e53b120d59c7696b3d34e
Empirous <166643596+Empirous@users.noreply.github.com>
2025-09-27 19:19:51 -0700

    Create trailing_zeroes.v

---
+++ ChipDev/Trailing Zeroes (Easy)/trailing_zeroes.v
@@ -0,0 +1,31 @@
+/* https://chipdev.io/question/12
+Prompt
+
+Find the number of trailing 0s in the binary representation of the input (din). If the input value is all 0s, the number of trailing 0s is the data width (DATA_WIDTH)
+Input and Output Signals
+
+    din - Input value
+    dout - Number of trailing 0s
+*/
+
+module model #(parameter
+  DATA_WIDTH = 32
+) (
+  input  [DATA_WIDTH-1:0] din,
+  output logic [$clog2(DATA_WIDTH):0] dout
+);
+
+  always_comb begin
+    
+    for(int i = 0; i < DATA_WIDTH; i++) begin
+      if(din == 0)
+        dout = DATA_WIDTH;
+      else if(din[i] == 1'b1) begin
+        dout = i;
+        break;
+      end
+    end
+    
+  end
+
+endmodule
