feff7ea5f0164f471723f86f0e457270de5c56ba
Empirous <166643596+Empirous@users.noreply.github.com>
2025-09-30 15:10:22 -0700

    Update programmable_sequence_detector.sv

--- ChipDev/Programmable Sequence Detector (Medium)/programmable_sequence_detector.sv
+++ ChipDev/Programmable Sequence Detector (Medium)/programmable_sequence_detector.sv
@@ -1,11 +1,15 @@
 /* https://chipdev.io/question/19
 Prompt
 
-Given a stream of input bits, pulse a 1 on the output (seen) whenever a specified 5-bit target sequence is detected on the input (din). The target sequence is specified by init and is updated whenever the reset-low signal (resetn) is deasserted.
+Given a stream of input bits, pulse a 1 on the output (seen) whenever a specified 5-bit target 
+sequence is detected on the input (din). The target sequence is specified by init and is updated 
+whenever the reset-low signal (resetn) is deasserted.
 
-The target sequence is always a 5-bit value, so left-padded 0's are also considered part of the sequence (a sequence specified as b11 would actually be b00011).
+The target sequence is always a 5-bit value, so left-padded 0's are also considered part of 
+the sequence (a sequence specified as b11 would actually be b00011).
 
 When resetn goes active, all previously seen bits on the input are no longer considered when searching for the target sequence.
+
 Input and Output Signals
 
     clk - Clock signal
