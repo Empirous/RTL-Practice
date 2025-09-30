2cb0252e45371e7711942b11505dd67805cc29a6
Empirous <166643596+Empirous@users.noreply.github.com>
2025-09-30 15:11:25 -0700

    Update serial2parallel_sr.sv

--- ChipDev/Serial-in, Parallel-out Shift Register (Easy)/serial2parallel_sr.sv
+++ ChipDev/Serial-in, Parallel-out Shift Register (Easy)/serial2parallel_sr.sv
@@ -2,9 +2,13 @@
 
 Prompt
 
-Build a circuit that reads a sequence of bits (one bit per clock cycle) from the input (din), and shifts the bits into the least significant bit of the output (dout). Assume all bits of the output are 0 to begin with.
+Build a circuit that reads a sequence of bits (one bit per clock cycle) from the input (din), 
+and shifts the bits into the least significant bit of the output (dout). 
+Assume all bits of the output are 0 to begin with.
 
-Once the number of input bits received is larger than DATA_WIDTH, only the DATA_WIDTH most recent bits are kept in the output.
+Once the number of input bits received is larger than DATA_WIDTH, only the DATA_WIDTH most 
+recent bits are kept in the output.
+
 Input and Output Signals
 
     clk - Clock signal
