eb221d672d66496891b05dc395c88c0f9fe06591
Empirous <166643596+Empirous@users.noreply.github.com>
2025-10-28 11:28:00 -0700

    Update simple_router_tb.vhd

--- ChipDev/Simple Router (Easy)/simple_router_tb.vhd
+++ ChipDev/Simple Router (Easy)/simple_router_tb.vhd
@@ -1,3 +1,5 @@
+-- https://www.edaplayground.com/x/b7pD
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 
@@ -26,14 +28,14 @@ port(
 end component;
 
 signal i_din    : std_logic_vector(DATA_WIDTH_C-1 downto 0);
-signal i_din_en : std_logic; 
+signal i_din_en : std_logic;
 signal i_addr   : std_logic_vector(1 downto 0);
 
 signal o_dout0, o_dout1, o_dout2, o_dout3: std_logic_vector(DATA_WIDTH_C-1 downto 0);
 
 begin
 
-  DUT: s_router port map(i_din, i_din_en, i_addr, o_dout0, o_dout1, o_dout2, o_dout3);
+  DUT: s_router generic map (DATA_WIDTH_C) port map(i_din, i_din_en, i_addr, o_dout0, o_dout1, o_dout2, o_dout3);
   
   process
   begin
@@ -41,73 +43,73 @@ begin
     i_din_en <= '1';
   	i_addr   <= "00";
     wait for 5 ns;
-    assert(o_dout0="11111111111111111111111111111111") report "Fail addr=00" severity error;
-    assert(o_dout1="00000000000000000000000000000000") report "Fail addr=00" severity error;
-    assert(o_dout2="00000000000000000000000000000000") report "Fail addr=00" severity error;
-    assert(o_dout3="00000000000000000000000000000000") report "Fail addr=00" severity error;
+    assert(o_dout0= (DATA_WIDTH_C-1 downto 0 => '1') ) report "Fail addr=00" severity error;
+    assert(o_dout1= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=00" severity error;
+    assert(o_dout2= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=00" severity error;
+    assert(o_dout3= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=00" severity error;
     
     i_din    <= (others => '1');
     i_din_en <= '1';
   	i_addr   <= "01";
     wait for 5 ns;
-    assert(o_dout0="00000000000000000000000000000000") report "Fail addr=01" severity error;
-    assert(o_dout1="11111111111111111111111111111111") report "Fail addr=01" severity error;
-    assert(o_dout2="00000000000000000000000000000000") report "Fail addr=01" severity error;
-    assert(o_dout3="00000000000000000000000000000000") report "Fail addr=01" severity error;
+    assert(o_dout0= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=01" severity error;
+    assert(o_dout1= (DATA_WIDTH_C-1 downto 0 => '1') ) report "Fail addr=01" severity error;
+    assert(o_dout2= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=01" severity error;
+    assert(o_dout3= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=01" severity error;
     
     i_din    <= (others => '1');
     i_din_en <= '1';
   	i_addr   <= "10";
     wait for 5 ns;
-    assert(o_dout0="00000000000000000000000000000000") report "Fail addr=10" severity error;
-    assert(o_dout1="00000000000000000000000000000000") report "Fail addr=10" severity error;
-    assert(o_dout2="11111111111111111111111111111111") report "Fail addr=10" severity error;
-    assert(o_dout3="00000000000000000000000000000000") report "Fail addr=10" severity error;
+    assert(o_dout0= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=10" severity error;
+    assert(o_dout1= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=10" severity error;
+    assert(o_dout2= (DATA_WIDTH_C-1 downto 0 => '1') ) report "Fail addr=10" severity error;
+    assert(o_dout3= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=10" severity error;
     
     i_din    <= (others => '1');
     i_din_en <= '1';
   	i_addr   <= "11";
     wait for 5 ns;
-    assert(o_dout0="00000000000000000000000000000000") report "Fail addr=11" severity error;
-    assert(o_dout1="00000000000000000000000000000000") report "Fail addr=11" severity error;
-    assert(o_dout2="00000000000000000000000000000000") report "Fail addr=11" severity error;
-    assert(o_dout3="11111111111111111111111111111111") report "Fail addr=11" severity error;
+    assert(o_dout0= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=11" severity error;
+    assert(o_dout1= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=11" severity error;
+    assert(o_dout2= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail addr=11" severity error;
+    assert(o_dout3= (DATA_WIDTH_C-1 downto 0 => '1') ) report "Fail addr=11" severity error;
     
     i_din    <= (others => '1');
     i_din_en <= '0';
   	i_addr   <= "00";
     wait for 5 ns;
-    assert(o_dout0="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout1="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout2="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout3="00000000000000000000000000000000") report "Fail din_en=0" severity error;
+    assert(o_dout0= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout1= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout2= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout3= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
     
     i_din    <= (others => '1');
     i_din_en <= '0';
   	i_addr   <= "01";
     wait for 5 ns;
-    assert(o_dout0="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout1="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout2="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout3="00000000000000000000000000000000") report "Fail din_en=0" severity error;
+    assert(o_dout0= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout1= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout2= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout3= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
     
     i_din    <= (others => '1');
     i_din_en <= '0';
   	i_addr   <= "10";
     wait for 5 ns;
-    assert(o_dout0="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout1="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout2="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout3="00000000000000000000000000000000") report "Fail din_en=0" severity error;
+    assert(o_dout0= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout1= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout2= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout3= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
     
     i_din    <= (others => '1');
     i_din_en <= '0';
   	i_addr   <= "11";
     wait for 5 ns;
-    assert(o_dout0="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout1="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout2="00000000000000000000000000000000") report "Fail din_en=0" severity error;
-    assert(o_dout3="00000000000000000000000000000000") report "Fail din_en=0" severity error;
+    assert(o_dout0= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout1= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout2= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
+    assert(o_dout3= (DATA_WIDTH_C-1 downto 0 => '0') ) report "Fail din_en=0" severity error;
   
   -- Clear inputs
   	i_din    <= (others => '0');
